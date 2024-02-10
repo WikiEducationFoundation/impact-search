5c02cd43efc43a2eb71fce7b566c173197423726
Amine Hassou <japnoo@gmail.com>
2024-02-10 03:23:06 +0100

    Added ui elements for category page

--- src/routes/WikipediaCategoryPage.tsx
+++ src/routes/WikipediaCategoryPage.tsx
@@ -1,5 +1,31 @@
-import React from "react";
+import { FormEvent, useState } from "react";
+import "./WikipediaCategoryPage.scss";
 
 export default function WikipediaCategoryPage() {
-  return <div>WikipediaCategoryPage</div>;
+  const [categoryURL, setCategoryURL] = useState<string>("");
+
+  const handleChange = (e) => {
+    setCategoryURL(e.target.value);
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="category-container">
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="text"
+          value={categoryURL}
+          onChange={handleChange}
+          placeholder="Enter a Category URL"
+          required
+        />
+        <button type="submit" className="submit-button">
+          Run Query
+        </button>
+      </form>
+    </div>
+  );
 }
