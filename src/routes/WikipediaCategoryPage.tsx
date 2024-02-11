f0f9f7bf0f3753bc67dcde78c6833d494318b67e
Amine Hassou <japnoo@gmail.com>
2024-02-11 03:18:57 +0100

    Added logic for fetching wikipedia categories, started work on treeview

--- src/routes/WikipediaCategoryPage.tsx
+++ src/routes/WikipediaCategoryPage.tsx
@@ -1,17 +1,56 @@
-import { FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import "./WikipediaCategoryPage.scss";
+import { MediaWikiResponse } from "../types";
+import CategoryTree from "../components/CategoryTree";
 
 export default function WikipediaCategoryPage() {
   const [categoryURL, setCategoryURL] = useState<string>("");
+  const [SubcatsData, setSubcatsData] = useState<MediaWikiResponse>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const handleChange = (e) => {
-    setCategoryURL(e.target.value);
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCategoryURL(event.target.value);
   };
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    const fetchedSubcats = await fetchInitialSubcats();
+    setSubcatsData(fetchedSubcats);
   };
 
+  async function fetchInitialSubcats(): Promise<MediaWikiResponse> {
+    setIsLoading(true);
+
+    let queriedSubcatsJSON: MediaWikiResponse;
+    const categoryTitle: string = categoryURL.split("/").slice(-1)[0];
+    try {
+      const urlParams: string = `action=query&generator=categorymembers&gcmlimit=130&gcmtype=subcat&prop=categoryinfo&gcmtitle=${categoryTitle}&format=json&origin=*`;
+      const response = await fetch(
+        `https://en.wikipedia.org/w/api.php?${urlParams}`,
+        {
+          headers: {
+            Accept: "application/sparql-results+json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok.");
+      }
+
+      queriedSubcatsJSON = await response.json();
+      if (queriedSubcatsJSON?.error) {
+        console.error(queriedSubcatsJSON?.error.info);
+      }
+    } catch (error) {
+      console.error("Error fetching subcats: ", error);
+      queriedSubcatsJSON = {};
+    }
+
+    setIsLoading(false);
+
+    return queriedSubcatsJSON;
+  }
   return (
     <div className="category-container">
       <form onSubmit={(e) => handleSubmit(e)}>
@@ -22,7 +61,7 @@ export default function WikipediaCategoryPage() {
           placeholder="Enter a Category URL"
           required
         />
-        <button type="submit" className="submit-button">
+        <button type="submit" className="submit-button" disabled={isLoading}>
           Run Query
         </button>
       </form>
--- src/types/index.ts
+++ src/types/index.ts
@@ -16,4 +16,33 @@ type SPARQLResponse = {
     }>;
   };
 };
-export type { SPARQLResponse };
+
+type MediaWikiResponse = {
+  batchcomplete: string;
+  continue?: {
+    gcmcontinue: string;
+    continue: string;
+  };
+  query: {
+    pages: {
+      [key: string]: {
+        pageid: number;
+        ns: number;
+        title: string;
+        categoryinfo: {
+          size: number;
+          pages: number;
+          files: number;
+          subcats: number;
+        };
+      };
+    };
+  };
+  error?: {
+    code: string;
+    info: string;
+    "*": string;
+  };
+  servedby?: string;
+};
+export type { SPARQLResponse, MediaWikiResponse };
